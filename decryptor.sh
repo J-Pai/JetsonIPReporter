c28cb924801dacf9a89d547891dbc26edfb9f050
j-pai <jesse.pai@gmail.com>
2018-03-03 18:34:07 -0500

    Included code that ensures git and other operations are done in repository's directory.

--- decryptor.sh
+++ decryptor.sh
@@ -3,15 +3,15 @@
 IP_ENC_FILE="curr_ip_enc.txt"
 IP_NOENC_FILE="curr_ip.txt"
 
+key=$(realpath $1)
+
 cd "$(dirname "$0")"
 
 git pull origin master
 
-echo $(pwd)
-
 if [ -f $IP_INC_FILE ]; then
   if [ $# -gt 0 ]; then
-    openssl rsautl -decrypt -inkey $1 -in $IP_ENC_FILE
+    openssl rsautl -decrypt -inkey $key -in $IP_ENC_FILE
   else
     echo "Must provide jetson_ip_reporter_key.pem file location."
   fi
